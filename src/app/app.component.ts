a994c9d48ea8d6d15933a76343d5f30fe2dfd70e
David Mansilla <you@example.com>
2020-12-04 08:31:13 -0600

    Angular code of the task #4 of the 7GUIs challenge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'timerTest';
+  progressValue: number = 0;
+  sliderValue: number = 0;
+  maxProgressValue: number = 30;
+  increaseValue: number = 0;
+
+  resetTimer() {
+    this.progressValue = 0;
+    this.maxProgressValue = this.sliderValue * 1000;
+
+    let interval = setInterval(() => {
+      this.progressValue = this.progressValue + 0.5;
+      this.increaseValue = this.progressValue / 5;
+    }, 100);
+
+    setTimeout(() => {
+      clearInterval(interval);
+    }, this.maxProgressValue);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatProgressBarModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
